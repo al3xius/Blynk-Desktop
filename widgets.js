b2f1de600909dcd6cf1feff7cf27a3dfd5590851
Alexander Schalk <al3xius.yt@gmail.com>
2019-05-24 14:21:41 +0200

    add Widget: Slider

--- widgets.js
+++ widgets.js
@@ -17,40 +17,21 @@ class Widget {
     }
 }
 
-class TABS {
-    constructor(properties) {
-        var tabs = properties.tabs
-        var dom = document.createElement("div")
-        dom.className = "col s12"
-
-        var ul = document.createElement("ul")
-        ul.className = "tabs"
-
-        for (let index = 0; index < tabs.length; index++) {
-            const tab = tabs[index]
-
-            var li = document.createElement("li")
-            li.className = "tab col s3"
-            var text = document.createElement("a")
-            text.href = "#tab" + index
-            text.innerHTML = tab.label
-            li.appendChild(text)
-            ul.appendChild(li)
-        }
-        dom.appendChild(ul)
-
-        return dom
-    }
-}
-
 class STYLED_BUTTON {
     constructor(properties, access) {
-        var dom = document.createElement("a")
-        dom.className = "waves-effect waves-light btn"
-        dom.id = "tab" + properties.tabId
-        var text = (properties.label || "") + " "
+        var dom = document.createElement("div")
+        var ul = document.createElement("ul")
+        var li1 = document.createElement("li")
+        var lable = document.createElement("label")
+        lable.innerHTML = properties.label || ""
+        li1.appendChild(lable)
+
+        var li2 = document.createElement("li")
+        var button = document.createElement("a")
+        button.className = "waves-effect waves-light btn"
+        button.id = "tab" + properties.tabId
         var offtext = properties.offButtonState.text || "OFF"
-        dom.innerHTML = text + offtext
+        button.innerHTML = offtext
 
         for (let index = 0; index < devices.length; index++) {
             if (devices[index].id == properties.deviceId) {
@@ -67,7 +48,7 @@ class STYLED_BUTTON {
                 var pin = "V" + properties.pin
         }
 
-        dom.onclick = function() {
+        button.onclick = function() {
             process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; //TODO: make more secure
             var currentValue = 0
 
@@ -103,16 +84,24 @@ class STYLED_BUTTON {
                 console.log("Error: " + err.message)
             })
         }
+        li2.appendChild(button)
+        ul.appendChild(li1)
+        ul.appendChild(li2)
+        dom.appendChild(ul)
         return dom
     }
 }
 
 class BUTTON {
     constructor(properties, access) {
-        var dom = document.createElement("a")
-        dom.className = "waves-effect waves-light btn"
-        dom.id = "tab" + properties.tabId
-        dom.innerHTML = "OFF"
+        var dom = document.createElement("div")
+        var lable = document.createElement("label")
+        lable.innerHTML = properties.label || ""
+
+        var button = document.createElement("a")
+        button.className = "waves-effect waves-light btn"
+        button.id = "tab" + properties.tabId
+        button.innerHTML = "OFF"
 
         for (let index = 0; index < devices.length; index++) {
             if (devices[index].id == properties.deviceId) {
@@ -129,7 +118,7 @@ class BUTTON {
                 var pin = "V" + properties.pin
         }
 
-        dom.onclick = function() {
+        button.onclick = function() {
             process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; //TODO: make more secure
             var currentValue = 0
 
@@ -165,6 +154,84 @@ class BUTTON {
                 console.log("Error: " + err.message)
             })
         }
+
+        dom.appendChild(lable)
+        dom.appendChild(button)
+        return dom
+    }
+}
+
+class SLIDER {
+    constructor(properties, access) {
+        var dom = document.createElement("div")
+        var ul = document.createElement("ul")
+        var li1 = document.createElement("li")
+        var lable = document.createElement("label")
+        lable.innerHTML = properties.label || ""
+        li1.appendChild(lable)
+
+        var li2 = document.createElement("li")
+        var slider = document.createElement("input")
+        slider.type = "range"
+        slider.min = properties.min
+        slider.max = properties.max
+            //dom.step = 1
+        slider.id = "tab" + properties.tabId
+
+        for (let index = 0; index < devices.length; index++) {
+            if (devices[index].id == properties.deviceId) {
+                var token = devices[index].token
+                break
+            }
+        }
+
+        switch (properties.pinType) {
+            case "DIGITAL":
+                var pin = "D" + properties.pin
+
+            case "VIRTUAL":
+                var pin = "V" + properties.pin
+        }
+
+        process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0; //TODO: make more secure
+        var currentValue = 0
+
+        //get current value
+        https.get("https://" + access.url + ":" + access.port + "/" + token + "/get/" + pin, (resp) => {
+            let data = ""
+
+            resp.on("data", (chunk) => {
+                data += chunk
+            })
+            resp.on("end", () => {
+                currentValue = JSON.parse(data)[0] || 0
+                slider.value = currentValue
+                return slider
+            })
+        }).on("error", (err) => {
+            console.log("Error: " + err.message)
+        })
+
+        slider.onclick = function() {
+            var value = slider.value
+
+            //send new Value
+            https.get("https://" + access.url + ":" + access.port + "/" + token + "/update/" + pin + "?value=" + value, (resp) => {
+                let data = ""
+
+                resp.on("data", (chunk) => {
+                    data += chunk
+                })
+                resp.on("end", () => {})
+            }).on("error", (err) => {
+                console.log("Error: " + err.message)
+            })
+        }
+
+        li2.appendChild(slider)
+        ul.appendChild(li1)
+        ul.appendChild(li2)
+        dom.appendChild(ul)
         return dom
     }
 }
@@ -179,8 +246,8 @@ function getAnchor() {
 function create(properties, access) {
     var type = properties.type
 
-    if (type == "testTABS") {
-        var widget = new TABS(properties)
+    if (type == "SLIDER") {
+        var widget = new SLIDER(properties, access)
     } else if (type == "STYLED_BUTTON") {
         var widget = new STYLED_BUTTON(properties, access)
     } else if (type == "BUTTON") {
